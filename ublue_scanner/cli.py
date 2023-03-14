606dea41e1f22fe7a07a489aeaaf6c5102184c41
Marco Ceppi <marco@ceppi.net>
2023-03-14 14:22:48 -0400

    fix: gh org package listing

--- ublue_scanner/cli.py
+++ ublue_scanner/cli.py
@@ -1,11 +1,10 @@
 import typer
 import arrow
 import re
+import httpx
 from enum import Enum
 from dotenv import load_dotenv
 from ghapi.all import GhApi
-from ghapi.core import print_summary
-from ghapi.page import paged
 from rich.table import Table
 from rich.console import Console
 from ruamel.yaml import YAML
@@ -43,6 +42,52 @@ def filter_tags(tags: list[str], include: list[str] = None, exclude: list[str] =
     return list(t)
 
 
+def get_github_packages(gh, org: str, **kwargs):
+    def _get(url):
+        return httpx.get(
+            url,
+            headers=gh.headers,
+            follow_redirects=True,
+        )
+
+    def _parse_link(link: str):
+        output = {}
+        if "," not in link:
+            return output
+
+        chunks = link.split(", ")
+        splitter = re.compile(r'^<(.*)>; rel="(.*)"$')
+        for chunk in chunks:
+            m = splitter.match(chunk)
+            if not m:
+                continue
+            output[m.group(2)] = m.group(1)
+        return output
+
+    query = ""
+    if kwargs:
+        _q = [f"{k}={v}" for k, v in kwargs.items()]
+        query = f"?{'&'.join(_q)}"
+
+    url = f"https://api.github.com/orgs/{org}/packages{query}"
+    has_next = True
+    packages = []
+
+    while url is not None:
+        req = _get(url)
+        try:
+            req.raise_for_status()
+        except:
+            break
+        url = None
+        packages.extend(req.json())
+
+        links = _parse_link(req.headers.get("link", ""))
+        url = links.get("next")
+
+    return packages
+
+
 @app.command()
 def scan(
     gh_token: str = typer.Option(
@@ -67,51 +112,47 @@ def scan(
             settings = yaml.load(f.read())
 
     gh = GhApi(token=gh_token)
-    paged_packages = paged(
-        gh.packages.list_packages_for_organization,
-        org=org,
-        package_type="container",
-        state="active",
-        per_page=1000,
-    )
-    seen_packages = []
-    has_pages = True
-    rows = []
+    # This should work, but paged / pages for list packages doesn't
+    # actually advance the page at all. It only ever pulls the first 30
+    # entries. However, if you curl the same endpoint directly it does
+    # work so I'm going to come back to this later and leave this issue
+    # which seems related: https://github.com/fastai/ghapi/issues/156
+    # paged_packages = paged(
+    #     gh.packages.list_packages_for_organization,
+    #     org=org,
+    #     package_type="container",
+    #     per_page=100,
+    # )
 
-    while has_pages:
-        page = next(paged_packages)
-        for package in page:
-            package_id = int(package["id"])
-            if package_id in seen_packages:
-                has_pages = False
+    packages = get_github_packages(gh, org=org, package_type="container")
+    rows = []
+    for package in packages:
+        owner = package["owner"]["login"]
+        owner_url = package["owner"]["html_url"]
+        image_name = package["name"]
+        repo_url = package["repository"]["html_url"]
+        repo_name = package["repository"]["name"]
+        repo_details = gh.repos.get(owner=owner, repo=repo_name)
+        repo_description = repo_details["description"]
+        stars = repo_details["stargazers_count"]
+        forks = repo_details["forks"]
+        updated_at = arrow.get(repo_details["updated_at"])
+
+        print(image_name)
+        if settings and len(settings["ignores"]) > 0:
+            if image_name in settings["ignores"]:
                 continue
 
-            seen_packages.append(package_id)
-            owner = package["owner"]["login"]
-            owner_url = package["owner"]["html_url"]
-            image_name = package["name"]
-            repo_url = package["repository"]["html_url"]
-            repo_name = package["repository"]["name"]
-            repo_details = gh.repos.get(owner=owner, repo=repo_name)
-            repo_description = repo_details["description"]
-            stars = repo_details["stargazers_count"]
-            forks = repo_details["forks"]
-            updated_at = arrow.get(repo_details["updated_at"])
-
-            if settings and len(settings["ignores"]) > 0:
-                if image_name in settings["ignores"]:
-                    continue
-
-            rows.append(
-                (
-                    image_name,
-                    repo_description,
-                    str(stars),
-                    str(forks),
-                    updated_at.humanize(),
-                    repo_url,
-                )
+        rows.append(
+            (
+                image_name,
+                repo_description,
+                str(stars),
+                str(forks),
+                updated_at.humanize(),
+                repo_url,
             )
+        )
 
     sorted_rows = sorted(rows, key=lambda tup: tup[0])
     if output == OutputOptions.cli:
